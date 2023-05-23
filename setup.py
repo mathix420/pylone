7261337b503a445e3126b347d86c1c3128dac313
Arnaud Gissinger <agissing@student.42.fr>
2023-05-23 11:30:10 +0200

    [BUMP] setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='pylone',
-    version='0.3.0',
+    version='0.3.1',
     description='A Python Serverless framework',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
